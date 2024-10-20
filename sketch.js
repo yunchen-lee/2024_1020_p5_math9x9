1bcaa64731794a90856fc417ec12fbb7a20e7156
yunchen-lee <101509731+yunchen-lee@users.noreply.github.com>
2024-10-20 22:21:12 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let x, y;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    background(220);
+    x = random([2, 3, 4, 5, 6, 7, 8, 9]);
+    y = random([2, 3, 4, 5, 6, 7, 8, 9]);
+
+
+}
+
+function draw() {
+    background(220);
+
+    push();
+    fill(0);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    textSize(200);
+    text(x + "x" + y, width / 2, height / 2);
+    pop();
+
+}
+
+function mouseClicked() {
+    x = random([2, 3, 4, 5, 6, 7, 8, 9]);
+    y = random([2, 3, 4, 5, 6, 7, 8, 9]);
+}
